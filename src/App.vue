e425c7a01cb218e09bca9813323f869795a57b4e
mmm1016 <1747508142@qq.com>
2021-04-13 17:34:11 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+<!--     $route 代表当前路由-->
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+
+  export default {
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+
+</style>
